3afa70984d6a20640a546f68cd7a15ae7687feec
Mayowa Makinde <Mayowamakinde53@gmail.com>
2017-03-14 18:32:27 +0100

    NaN correction effected

--- app/words.js
+++ app/words.js
@@ -3,7 +3,7 @@ words :function(str){
   var dict = {};
   var arrOfWord = str.split(/\s+/g);
   for (i=0; i<= arrOfWord.length - 1; i++){
-    if (dict[arrOfWord[i]]){
+    if (dict[arrOfWord[i]] && !isNaN(dict[arrOfWord[i]])){
       dict[arrOfWord[i]]++
     }
     else{
